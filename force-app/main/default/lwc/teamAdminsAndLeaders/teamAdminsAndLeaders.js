2d5e6566d5220b23367a087cd8996e7b827316d0
Dawn Kunig <dawn.kunig@fluidigm.com>
2020-10-22 13:25:09 -0400

    Final updates pushing to production

--- force-app/main/default/lwc/teamAdminsAndLeaders/teamAdminsAndLeaders.js
+++ force-app/main/default/lwc/teamAdminsAndLeaders/teamAdminsAndLeaders.js
@@ -4,17 +4,20 @@ import getLeaders from '@salesforce/apex/litmosTeamsController.getLeaders';
 
 export default class TeamAdminsAndLeaders extends LightningElement {
     @api recordId;
-    @track admins;
-    @track leaders;
+    @track admins = undefined;
+    @track leaders = undefined;
     @track adminError;
     @track leaderError;
     connectedCallback() {
         console.log('connected callback');
         getAdmins({recordID:this.recordId})
         .then(result => {
-            console.log('result: ' + result);
-            this.admins = result;
-            console.log('admins: ' + this.admins);
+            if(result.length != 0) {
+                console.log('result: ' + result);
+                this.admins = result;
+                console.log('admins: ' + this.admins);
+            } else {this.admins=undefined;}
+            
         })
         .catch(error => {
             this.adminError = error;
@@ -23,8 +26,12 @@ export default class TeamAdminsAndLeaders extends LightningElement {
         })
         getLeaders({recordID:this.recordId})
         .then(result => {
-            console.log('leaders: ' + result);
-            this.leaders = result;
+            if(result.length > 0) {
+                console.log('leaders: ' + result);
+                this.leaders = result;
+                this.leaderError = undefined;
+            } else {this.leaders = undefined;}
+            
         })
         .catch(error => {
             this.leaderError = error;
